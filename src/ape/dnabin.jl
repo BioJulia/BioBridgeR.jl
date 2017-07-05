373036e236bd202cb0a87e662ebd5bfde9c1742f
Ben J. Ward <ward9250@gmail.com>
2017-07-05 11:25:14 +0100

    Change to new parametric method syntax fix

--- src/ape/dnabin.jl
+++ src/ape/dnabin.jl
@@ -61,7 +61,7 @@ end
     return swapbits(x, 4, 7) >> 4
 end
 
-@inline function Base.convert(::Type{BioSymbols.DNA}, nt::DNAbin)
+@inline function Base.convert(::Type{DNA}, nt::DNAbin)
     return BioSymbols.DNA(_raw_to_DNA(UInt8(nt)))
 end
 
@@ -158,7 +158,7 @@ end
     end
 end
 
-function rcopy(::Type{Array{DNAbin,2}}, rs::Ptr{RawSxp})
+function rcopy(::Type{Array{DNAbin, 2}}, rs::Ptr{RawSxp})
     # Check class of RawSxp.
     check_class(rs)
 
@@ -217,7 +217,7 @@ function sexp(::Type{RawSxp}, seq::Array{DNAbin,2})
     return ra
 end
 
-function sexp{A<:DNAAlphabet}(::Type{RawSxp}, seq::Vector{BioSequence{A}})
+function sexp(::Type{RawSxp}, seq::Vector{BioSequence{A}}) where {A<:DNAAlphabet}
     ncols = length(seq[1])
     @inbounds for i ∈ 2:endof(seq)
         if length(seq[i]) != ncols
@@ -235,4 +235,4 @@ function sexp{A<:DNAAlphabet}(::Type{RawSxp}, seq::Vector{BioSequence{A}})
     return ra
 end
 
-sexp(seq::Vector{BioSequence{A}}) = sexp(RawSxp, seq)
+sexp(seq::Vector{BioSequence{A}}) where {A<:DNAAlphabet} = sexp(RawSxp, seq)
